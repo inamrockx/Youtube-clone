37abdb9fb8421f225030801e9e5735935a9ef4f6
Inam Ullah <khan112261@gmail.com>
2026-08-01 02:31:26 +0500

    implement MongoDB connection and configure environment variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log("Helllllloooo")
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./.env'
+})
+
+connectDB()
+
+
+ 
+
+
+
+
+
+
+
+// rough method
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants";
+// import express from "express";
+// const app = express();
+
+
+// ( async ()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on("error" , (error)=>{
+//             console.log("ERR:", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is running on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR:" , error)
+//         throw error
+        
+//     }
+// })()
