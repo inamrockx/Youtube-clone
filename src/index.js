6307d583be4f9ea5e5694269f438728f098a9690
Inam Ullah <khan112261@gmail.com>
2026-08-04 02:34:29 +0500

    initialize express application and implement database logic

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import {app} from './app.js';
+import { error } from "node:console";
 
 dotenv.config({
     path:'./.env'
 })
 
 connectDB()
+.then(
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    })
+).catch((error) => {
+    console.log('MONGODB CONNECTION FAILED !!' , error)
+})
 
 
  
